dd803c93df8b184e8db42d167a978a8746ee4c17
Aleksandr Belousov <aleksandr.b@smplt.net>
2022-07-26 18:11:40 +0300

    fixed

---
+++ src/Service.BuyCryptoProcessor.Postgres/Migrations/20220726151122_CardType.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.BuyCryptoProcessor.Postgres.Migrations
+{
+    public partial class CardType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CardType",
+                schema: "cryptobuy",
+                table: "intentions",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CardType",
+                schema: "cryptobuy",
+                table: "intentions");
+        }
+    }
+}
